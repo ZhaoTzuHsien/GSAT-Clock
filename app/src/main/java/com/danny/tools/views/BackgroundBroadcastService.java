fda3b0cc0485791e33b64d44275918951947f5c5
Zhao Tzu-Hsien <danny900714@gmail.com>
2018-09-11 22:30:07 +0800

    1.1.3.0002

---
+++ app/src/main/java/com/danny/tools/views/BackgroundBroadcastService.java
@@ -0,0 +1,15 @@
+package com.danny.tools.views;
+import android.app.*;
+import android.content.*;
+import android.os.*;
+
+public class BackgroundBroadcastService extends Service
+{
+
+	@Override
+	public IBinder onBind(Intent p1) {
+		// TODO: Implement this method
+		return null;
+	}
+
+}
